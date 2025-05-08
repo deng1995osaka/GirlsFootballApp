9f771a6265ae7fd91ac966637316c52a85cd84c6
deng1995osaka <addressee3@gmail.com>
2025-05-08 18:07:17 +0900

    Initial commit: Girls Football App

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,20 @@
 const { getDefaultConfig } = require('@expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = config; 
+config.resolver.alias = {
+  '@lib': path.resolve(__dirname, 'src/lib'),
+  '@hooks': path.resolve(__dirname, 'src/hooks'),
+  '@utils': path.resolve(__dirname, 'src/utils'),
+  '@components': path.resolve(__dirname, 'src/components'),
+  '@screens': path.resolve(__dirname, 'src/screens'),
+  '@styles': path.resolve(__dirname, 'src/styles'),
+  '@constants': path.resolve(__dirname, 'src/constants'),
+  '@assets': path.resolve(__dirname, 'assets'),
+  '@store': path.resolve(__dirname, 'src/store'),
+  '@services': path.resolve(__dirname, 'src/services'),
+  '@config': path.resolve(__dirname, 'src/config'),
+};
+
+module.exports = config;
